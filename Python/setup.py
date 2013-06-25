f1bedbd910db704e294532d301ebcc0919dd91c0
Daryl Dudey <daryljdudey@googlemail.com>
2013-06-25 11:32:32 +0100

    ..

--- Python/setup.py
+++ Python/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-ext_modules = [Extension("greedycythoncalc", ["distanceBetweenPointsLatLong.pyx"])]
+ext_modules = [Extension("greedycythoncalc", ["greedycythoncalc.pyx"])]
 
 setup(
   name = 'Greedy Performance Test',
