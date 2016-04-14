c02254807763ebcef4ca05368f6a3f3a149beb9f
= <=>
2016-04-14 11:35:10 +0100

    Rust

--- Rust/greedy.rs
+++ Rust/greedy.rs
@@ -1,10 +1,11 @@
-// rustc -C opt-level=3 -C target-cpu=core2 greedy.rs
+// rustc -C opt-level=3 -C target-cpu=native greedy.rs
 
-use std::old_io::File;
+use std::io::prelude::*;
+use std::fs::File;
 use std::str::FromStr;
 use std::f64;
 use std::f64::consts::PI;
-use std::num::Float;
+use std::path::Path;
 
 static EARTH_RADIUS_M:f64 = 6367450.0;
 static CONVERT_RAD:f64 = PI / 180.0;
@@ -66,14 +67,14 @@ impl SchemaData {
 fn build(lines: Vec<&str>) -> SchemaData {
     let mut sd = SchemaData::new();
     for x in lines.iter() {
-        let split = x.split_str(",").collect::<Vec<&str>>();
+        let split = x.split(',').collect::<Vec<_>>();
         let id:&str = split[0];
-        let lat: f64 = FromStr::from_str(split[1]).unwrap();
-        let lng: f64 = FromStr::from_str(split[2]).unwrap();
+        let lat: f64 = split[1].parse::<f64>().unwrap();
+        let lng: f64 = split[2].parse::<f64>().unwrap();
         if split.len() == 3 {
-            sd.resource.push(Resource::new(&String::from_str(id), lat, lng));
+            sd.resource.push(Resource::new(&String::from_str(id).unwrap(), lat, lng));
         } else if split.len() == 4 {
-            sd.activity.push(Activity::new(&String::from_str(id), lat, lng));
+            sd.activity.push(Activity::new(&String::from_str(id).unwrap(), lat, lng));
         }
     }
     sd
@@ -86,10 +87,10 @@ fn distance_between_points(lat1:f64, lon1:f64, lat2:f64, lon2:f64) -> f64 {
     let end_lon_in_rad: f64 = lon2 * CONVERT_RAD;
     let longitude: f64 = end_lon_in_rad - start_lon_in_rad;
     let latitude: f64 = end_lat_in_rad - start_lat_in_rad;
-    let sin_half_lat: f64 = Float::sin(latitude * 0.5);
-    let sin_half_lon: f64 = Float::sin(longitude * 0.5);
-    let a: f64 = sin_half_lat * sin_half_lat + Float::cos(start_lat_in_rad) * Float::cos(end_lat_in_rad) * sin_half_lon * sin_half_lon;
-    let c: f64 = Float::atan2(Float::sqrt(a), Float::sqrt(1.0 - a));
+    let sin_half_lat: f64 = (latitude * 0.5).sin();
+    let sin_half_lon: f64 = (longitude * 0.5).sin();
+    let a: f64 = sin_half_lat * sin_half_lat + (start_lat_in_rad).cos() * (end_lat_in_rad).cos() * sin_half_lon * sin_half_lon;
+    let c: f64 = a.sqrt().atan2((1.0 - a).sqrt());
     EARTH_RADIUS_M * (c + c)
 }
 
@@ -131,9 +132,11 @@ fn schedule_resources(sd: &mut SchemaData) -> f64 {
 
 fn main() {
     // Load lines
-    let mut file = File::open(&Path::new("/Users/daryl/Development/Projects/FunctionalComparison/Data/DataSPIF.csv"));
-    let contents = file.read_to_end().unwrap();
-    let filestr = String::from_utf8(contents).unwrap();
+    let mut file = File::open(Path::new("D:/Development/FunctionalComparison/Data/DataSPIF.csv")).unwrap();
+    //let mut file = File::open(Path::new("/Users/daryl/Development/Projects/FunctionalComparison/Data/DataSPIF.csv")).unwrap();
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).unwrap();
+    let filestr = String::from_utf8(buffer).unwrap();
     let lines: Vec<&str> = filestr.lines().collect();
 
     // Build
