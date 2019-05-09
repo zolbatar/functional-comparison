63adeb2ef60f8fcc48331429f7dbbd31b50f87ce
Daryl Dudey <daryljdudey@googlemail.com>
2019-05-09 22:24:04 +0100

    latest

--- RunRuby.sh
+++ RunRuby.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+source ~/.rvm/scripts/rvm
 
 # Ruby
 cd Ruby
@@ -9,7 +10,6 @@ rvm use ruby
 echo "TruffleRuby: " > ../Timings/TruffleRuby.txt
 rvm use truffleruby
 /usr/bin/time -v ruby greedy.rb 2>> ../Timings/TruffleRuby.txt 
-cd ..
 echo "JRuby: " > ../Timings/JRuby.txt
 rvm use jruby
 /usr/bin/time -v ruby greedy.rb 2>> ../Timings/JRuby.txt 
